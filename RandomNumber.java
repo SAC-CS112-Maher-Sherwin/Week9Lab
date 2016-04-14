48333383351c21b70a2ac09ae353993637f35e31
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-04-13 20:43:45 -0700

    Update RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,10 +1,13 @@
+package guessanumber;
+
 public class RandomNumber {
     
+    int i = 1 + (int) (Math.random() * 10);
+            
     public int GetANumber_Between_1_and_10() {
         
-        int i = 1 + (int) (Math.random() * 10);
-        System.out.println(i);
         return i;
         
     }
 }
+
