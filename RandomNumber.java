ace492ae38ff2c7036570e977e0be930a15c3835
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-04-13 20:10:26 -0700

    Added files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,12 @@
+package newguessanumber;
+
+public class RandomNumber {
+    
+    public int GetANumber_Between_1_and_10() {
+        
+        int i = 1 + (int) (Math.random() * 10);
+        System.out.println(i);
+        return i;
+        
+    }
+}
